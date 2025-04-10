db34bc1dfc2a17c3dda2b294ab32ee417b836891
SlavkoMuzdeka <muzdekaslavko@gmail.com>
2025-04-10 16:10:59 +0200

    Added some changes to initial version

--- components/home-section.tsx
+++ components/home-section.tsx
@@ -36,7 +36,7 @@ const HomeSection: React.FC<HomeSectionProps> = ({
             className="flex flex-col gap-6"
           >
             <div className="inline-flex flex-wrap gap-2 px-4 py-1 rounded-md bg-slate-800 border border-slate-700 text-slate-300 text-sm font-mono">
-              <span className="text-blue-400">const</span> softwareEng = {"{"}
+              <span className="text-blue-400">const</span> dev = {"{"}
               <span className="text-green-400">"Full Stack"</span>,
               <span className="text-green-400">"Blockchain"</span>
               <span className="text-green-400">"AI"</span>
